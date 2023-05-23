70e41729131dbf6711d82ef7b52ad88de4881245
SHRIYAM SHRIVASATVA <shrivasatvashriyam2@gmail.com>
2023-05-23 21:38:14 +0530

    HTML

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const [tableChecked,setTableChecked] = useState({
+    json:false,
+    table:false
+  });
+
+  const isChecked = (e)=>{
+    let checkDetails = e?.target
+    if(checkDetails?.value === 'JSON' && checkDetails?.checked === true){
+      setTableChecked({json:true,table:false})
+    }else if(checkDetails?.value === 'Table' && checkDetails?.checked === true){
+      setTableChecked({json:false,table:true})
+    }else{
+      setTableChecked({json:false,table:false})
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <header>
+      <button>Upload File or Drag a drop</button>
+    </header>
+    <main>
+      <div className='selection'>
+      <input type='checkbox' value={'JSON'} name='data' onChange={isChecked} checked={tableChecked?.json}/><span>JSON</span>
+      <input type="checkbox" value={'Table'} name ='data' onChange={isChecked} checked={tableChecked?.table}/><span>Table</span>
+      </div>
+      <main>
+        {tableChecked?.json?
+        <div>JSON DATA</div>
+        : tableChecked?.table ?
+        <div>Table</div>
+        : <div>Please select one option</div>
+      }
+      </main>
+    </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
